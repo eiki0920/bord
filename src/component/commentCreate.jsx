b531e0c2247348c315ab45f42516744b6d2ca14d
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-09-27 14:15:18 +0900

    コメントを空白で送れないように

--- src/component/commentCreate.jsx
+++ src/component/commentCreate.jsx
@@ -4,33 +4,34 @@ export const CommentCreate = (props) => {
   const [text, setText] = useState("");
 
   const onClickButton = () => {
-    fetch(
-      `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${props.threadId}/posts`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    if (text) {
+      fetch(
+        `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${props.threadId}/posts`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            post: text,
+          }),
         },
-        body: JSON.stringify({
-          post: text,
-        }),
-      },
-      console.log(props.threadId)
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setText("");
+        console.log(props.threadId)
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setText("");
 
-        console.log(data);
-
-        //ページ再読み込み
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.log("エラーが発生しました");
-      });
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log("エラーが発生しました");
+        });
+    } else {
+      alert("コメントを入力してください");
+    }
   };
 
   return (
